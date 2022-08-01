8038409714b9452b1237194ef7d69b8960d0d21d
ACER <Rana-MH-1>
2022-08-01 23:03:31 +0100

    toDoLidt with Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {useSelector} from 'react-redux'
+import TodoList from './Components/TodoList';
+import AddTodos from './Components/AddTodo';
+import Filtered from './Components/Filter';
 
 function App() {
+  let todos = useSelector(state=> state.Todo.todos)
+  let Filter = useSelector(state=> state.Todo.Filter)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>TodoList</h1>
+     <hr/>
+     <AddTodos/>
+     <TodoList todos={(Filter=='Done')? todos.filter(el=> el.isDone== true): (Filter=='UnDone')? todos.filter(el=> el.isDone==false): todos} />
+      <hr/>
+      <Filtered/>
+
     </div>
   );
 }
